bd3246c0ad65d6ea94fc8372b8bb8d3373cfcda0
sahar <saharnajmus91@gmail.com>
2023-03-12 18:01:42 +0100

    Initial commit

---
+++ rest/src/main/java/be/techfix/rest/controller/MyShoppingRestController.java
@@ -0,0 +1,13 @@
+package be.techfix.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyShoppingRestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+}
